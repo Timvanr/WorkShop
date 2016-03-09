b54d2225a2038919bbee881e445ef761b1291602
= <=>
2016-03-09 17:35:18 +0100

    Klassen + DAO's + Randomize Klanten
    Dit zijn de drie klassen (Klant, Adres, Bestelling) en de DAO's voor
    klant/adres en bestelling. Ik wist niet of het handig was om voor adres
    ook nog een aparte DAO te maken dus ik heb dit nu nog maar bij klant
    gelaten.

---
+++ Artikel.java
@@ -0,0 +1,28 @@
+
+public class Artikel {
+
+	private int artikel_id;
+	private String artikel_naam;
+	private String artikel_prijs;
+	
+	public int getArtikel_id() {
+		return artikel_id;
+	}
+	public void setArtikel_id(int artikel_id) {
+		this.artikel_id = artikel_id;
+	}
+	public String getArtikel_naam() {
+		return artikel_naam;
+	}
+	public void setArtikel_naam(String artikel_naam) {
+		this.artikel_naam = artikel_naam;
+	}
+	public String getArtikel_prijs() {
+		return artikel_prijs;
+	}
+	public void setArtikel_prijs(String artikel_prijs) {
+		this.artikel_prijs = artikel_prijs;
+	}
+	
+	
+}
