6a7c5678933f2ee98cdeadee8f815fa1cde1b050
Maurice555 <mauricevanh555@yahoo.com>
2016-03-17 12:51:41 +0100

    Update AdresDAO.java

--- AdresDAO.java
+++ AdresDAO.java
@@ -4,16 +4,16 @@ import java.sql.SQLException;
 public interface AdresDAO {
 	
 	//static connection getConnection(String user, String password) throws SQLException; evt inloggen met DBnaam?
-	void createAdres(Adres adres) throws SQLException;
-	Adres readAdres(int id) throws SQLException;//optioneel met toString()
-	void deleteAdres(int id) throws SQLException;
-	Hashset<Adres> readAll() throws SQLException;//of set of arraylist?
-	Adres searchById(int id) throws SQLException;
-	int searchByPostcodeAndHuisnummer(String postcode, int huisnummer, String toevoeging) throws SQLException;//moet ook werken als toevoeging null is
-	Hashset<Adres> searchByStraat(String straat) throws SQLException;
-	Hashset<Adres> searchByStraatAndHuisnummer(String straat, int huisnummer, String toevoeging) throws SQLException;//kan bijvoorbeeld searchByStraat() callen
-	Hashset<Adres> getKlant(int id) throws SQLException;
-	void close() throws SQLException;	
+	void createAdres(Adres adres) throws SQLException;//Sander
+	Adres readAdres(int id) throws SQLException;//Tim//optioneel met toString()
+	void deleteAdres(int id) throws SQLException;//Tim
+	Hashset<Adres> readAll() throws SQLException;//Tim//of set of arraylist?
+	Adres searchById(int id) throws SQLException;//Maurice
+	int searchByPostcodeAndHuisnummer(String postcode, int huisnummer, String toevoeging) throws SQLException;//Sander//moet ook werken als toevoeging null is
+	Hashset<Adres> searchByStraat(String straat) throws SQLException;//Maurice
+	Hashset<Adres> searchByStraatAndHuisnummer(String straat, int huisnummer, String toevoeging) throws SQLException;//Maurice//kan bijvoorbeeld searchByStraat() callen
+	Hashset<Adres> getKlant(int id) throws SQLException;//Sander
+	void close() throws SQLException;//Maurice	
 }
 /*
 Gerbrich vind dat iedere methode moet beginnen met getConnection() en moet eindigen met connection.close().
