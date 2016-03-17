9b5087f5908888ab9590742859ba962d54a8ed53
= <= sandermgns@hotmail.com>
2016-03-17 17:28:50 +0100

    createPlusMenu
    Zit weer in de verkeerde branch maar dit zijn de aanpassingen van het
    create menu. Bij de vorige commit had ik iets verkeerd laten staan dus
    daarom had ik die verwijderd

--- KeuzeMenu.java
+++ KeuzeMenu.java
@@ -138,270 +138,36 @@ public class KeuzeMenu {
 		}
 	}
 
-
-	private void createKlantMenu() {
-		System.out.println("SELECTEER EEN CREATE METHODE");
-		System.out.println("");
-		System.out.println("1. Create klant");
-		System.out.println("2. Create klant & adres");
-		System.out.println("3. Create klant, adres & bestelling");
-		System.out.println("4. Create klant & bestelling");
-		System.out.println("");
-		System.out.print("Maak een keuze: ");
-		int keuze = input.nextInt();
-		switch (keuze){
-		case 1:
-			createKlant();
-			break;
-		case 2:
-			createKlantAdres();
-			break;
-		case 3:
-			createKlantAdresBestelling();
-			break;
-		case 4:
-			createKlantBestelling();
-			break;
-		default:
-			System.out.println("Ongeldige keuze!");
-			createKlantMenu();
-			break;
+		private void createKlantMenu() {
+			System.out.println("SELECTEER EEN CREATE METHODE");
+			System.out.println("");
+			System.out.println("1. Create klant");
+			System.out.println("2. Create klant & adres");
+			System.out.println("3. Create klant, adres & bestelling");
+			System.out.println("4. Create klant & bestelling");
+			System.out.println("");
+			System.out.print("Maak een keuze: ");
+			int keuze = input.nextInt();
+			switch (keuze){
+			case 1:
+				klantDAO.createKlant();;
+				break;
+			case 2:
+				klantDAO.createKlantNaamEnAdres();
+				break;
+			case 3:
+				klantDAO.createKlantEnAdresEnBestelling();
+				break;
+			case 4:
+				klantDAO.createKlantEnBestelling();
+				break;
+			default:
+				System.out.println("Ongeldige keuze!");
+				createKlantMenu();
+				break;
+			}
 		}
-	}
-
-	private void createKlant() {
-		//Klant gegevens
-		System.out.println("Vul de gevraagde gegevens correct in: ");
-		System.out.print("Voornaam: ");
-		String voornaam = input.next();
-		System.out.print("Tussenvoegsel: ");
-		String tussenvoegsel = input.next();
-		System.out.print("Achternaam: ");
-		String achternaam = input.next();
-		System.out.print("Emailadres: ");
-		String email = input.next();
-
-		klant.setVoornaam(voornaam);
-		klant.setTussenvoegsel(tussenvoegsel);
-		klant.setAchternaam(achternaam);
-		klant.setEmail(email);
-
-		klantDAO.createKlant(klant);
-		System.out.println(klant.toString() + " is aangemaakt");
-	}
 	
-	private void createKlantAdres() {
-		//Klant gegevens:
-		System.out.print("Voornaam: ");
-		String voornaam = input.next();
-		System.out.print("Tussenvoegsel: ");
-		String tussenvoegsel = input.next();
-		System.out.print("Achternaam: ");
-		String achternaam = input.next();
-		System.out.print("Emailadres: ");
-		String email = input.next();
-		//Adres gegevens: 
-		System.out.print("Straatnaam: ");
-		String straatnaam = input.next();
-		System.out.print("Postcode: ");
-		String postcode = input.next();
-		System.out.print("Toevoeging: ");
-		String toevoeging = input.next();
-		System.out.print("Huisnummer: ");
-		int huisnummer = input.nextInt();
-		System.out.print("Woonplaats: ");
-		String woonplaats = input.next();
-
-		klant.setVoornaam(voornaam);
-		klant.setTussenvoegsel(tussenvoegsel);
-		klant.setAchternaam(achternaam);
-		klant.setEmail(email);
-		klant.adres.setStraatnaam(straatnaam);
-		klant.adres.setPostcode(postcode);
-		klant.adres.setToevoeging(toevoeging);
-		klant.adres.setHuisnummer(huisnummer);
-		klant.adres.setWoonplaats(woonplaats);
-
-
-		klantDAO.createKlant(klant);
-		System.out.println(klant.toString() + klant.adres.toString() 
-		+ " is aangemaakt");
-	}
-
-	private void createKlantAdresBestelling() {
-		//Klant gegevens:
-		System.out.print("Voornaam: ");
-		String voornaam = input.next();
-		System.out.print("Tussenvoegsel: ");
-		String tussenvoegsel = input.next();
-		System.out.print("Achternaam: ");
-		String achternaam = input.next();
-		System.out.print("Emailadres: ");
-		String email = input.next();
-		//Adres gegevens: 
-		System.out.print("Straatnaam: ");
-		String straatnaam = input.next();
-		System.out.print("Postcode: ");
-		String postcode = input.next();
-		System.out.print("Toevoeging: ");
-		String toevoeging = input.next();
-		System.out.print("Huisnummer: ");
-		int huisnummer = input.nextInt();
-		System.out.print("Woonplaats: ");
-		String woonplaats = input.next();
-
-		klant.setVoornaam(voornaam);
-		klant.setTussenvoegsel(tussenvoegsel);
-		klant.setAchternaam(achternaam);
-		klant.setEmail(email);
-		klant.adres.setStraatnaam(straatnaam);
-		klant.adres.setPostcode(postcode);
-		klant.adres.setToevoeging(toevoeging);
-		klant.adres.setHuisnummer(huisnummer);
-		klant.adres.setWoonplaats(woonplaats);
-
-		//Bestelling gegevens
-		int keuze = 1;
-		while (keuze == 1)  {
-			System.out.println("Welke artikel wilt u toevoegen: ");
-			System.out.println("Artikel id:");
-			int id = input.nextInt();
-			System.out.print("Artikel naam: ");
-			String artikelnaam = input.next();
-			System.out.print("Hoeveel wilt u van dit artkel: ");
-			int aantal = input.nextInt();
-			System.out.print("Wat is de prijs van dit artikel: ");
-			double prijs = input.nextDouble();
-
-			Artikel artikelAdd = new Artikel(id, artikelnaam, prijs, aantal);
-			klant.bestelling.voegArtikelToeAanBestelling(artikelAdd);
-			System.out.print("Wilt u meer artikelen toevoegen (kies 1 voor ja en 2 voor nee)? ");
-			keuze = input.nextInt();
-		}
-		klantDAO.createKlant(klant);
-	}
-
-	private void createKlantBestelling() {
-		//Klant gegevens: 
-		System.out.print("Voornaam: ");
-		String voornaam = input.next();
-		System.out.print("Tussenvoegsel: ");
-		String tussenvoegsel = input.next();
-		System.out.print("Achternaam: ");
-		String achternaam = input.next();
-		System.out.print("Emailadres: ");
-		String email = input.next();
-
-		klant.setVoornaam(voornaam);
-		klant.setTussenvoegsel(tussenvoegsel);
-		klant.setAchternaam(achternaam);
-		klant.setEmail(email);
-
-		//Bestelling gegevens
-		int keuze = 1;
-		while (keuze == 1)  {
-			System.out.println("Welke artikel wilt u toevoegen: ");
-			System.out.println("Artikel id:");
-			int id = input.nextInt();
-			System.out.print("Artikel naam: ");
-			String artikelnaam = input.next();
-			System.out.print("Hoeveel wilt u van dit artkel: ");
-			int aantal = input.nextInt();
-			System.out.print("Wat is de prijs van dit artikel: ");
-			double prijs = input.nextDouble();
-
-			Artikel artikelAdd = new Artikel(id, artikelnaam, prijs, aantal);
-			klant.bestelling.voegArtikelToeAanBestelling(artikelAdd);
-			System.out.print("Wilt u meer artikelen toevoegen (kies 1 voor ja en 2 voor nee)? ");
-			keuze = input.nextInt();
-		}
-		klantDAO.createKlant(klant);
-	}
-	
-	private void readKlantMenu() {
-		System.out.println("");
-		System.out.println("SELECTEER EEN READ METHODE");
-		System.out.println("");
-		System.out.println("1. Read klant met ID-nummer");
-		System.out.println("2. Read klant met voornaam");
-		System.out.println("3. Read klant met volledige naam");
-		System.out.println("4. Read klant met adres");
-		System.out.println("5. Read klant met straatnaam");
-		System.out.println("6. Read klant met postcode en huisnummer");
-		System.out.println("");
-		System.out.print("Maak een keuze: ");
-		int keuze = input.nextInt();
-		switch (keuze){
-		case 1:
-			System.out.println("Geef uw klant ID:");
-			int id = input.nextInt();
-			try {
-				klantDAO.readKlantWithId(id);
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			break;
-		case 2:
-			System.out.println("Geef u voornaam: ");
-			String voornaam = input.next();
-			try {
-				klantDAO.readKlantWithFirstName(voornaam);
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			break;
-		case 3:
-			System.out.println("Geef u volledige naam: ");
-			String volledigeNaam = input.nextLine();
-			try {
-				klantDAO.readKlantWithFirstName(volledigeNaam);
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			break;
-		case 4:
-			System.out.print("Straatnaam: ");
-			String straatnaam = input.next();
-			System.out.print("Postcode: ");
-			String postcode = input.next();
-			System.out.print("Toevoeging: ");
-			String toevoeging = input.next();
-			System.out.print("Huisnummer: ");
-			int huisnummer = input.nextInt();
-			System.out.print("Woonplaats: ");
-			String woonplaats = input.next();
-			try {
-				klantDAO.readKlantWithAddress(straatnaam, huisnummer, toevoeging, postcode, woonplaats);
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			break;
-		case 5:
-			System.out.println("Geef uw straatnaam: ");
-			String straatnaam1 = input.next();
-			try {
-				klantDAO.readKlantWithStraatnaam(straatnaam1);
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			break;
-		case 6:
-			System.out.print("Geef uw Postcode: ");
-			String postcode1 = input.next();
-			System.out.print("Huisnummer: ");
-			int huisnummer1 = input.nextInt();
-			try {
-				klantDAO.readKlantWithPostCodeHuisnummer(postcode1, huisnummer1);
-			}
-			catch (SQLException e) {
-				e.printStackTrace();
-			}
-			break;
-		default:
-			System.out.println("Ongeldige keuze!");
-			readKlantMenu();
-			break;
-		}
 	}
 	
 	private void updateKlantMenu(){
