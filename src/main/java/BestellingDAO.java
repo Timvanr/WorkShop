5c4aa035805b37b2581396091efcd9659b9bd143
Maurice555 <mauricevanh555@yahoo.com>
2016-04-11 20:16:32 +0200

    nu zonder throws

---
+++ src/main/java/BestellingDAO.java
@@ -0,0 +1,17 @@
+
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public interface BestellingDAO {
+	
+	//void maakTabel() throws SQLException ;
+	void voegBestellingToe(Bestelling bestelling);
+	Bestelling getBestelling(int id);
+	int haalKlant_id(int bestelling_id);
+	void updateBestelling(int bestelling_id, Bestelling bestelling);
+	void verwijderBestelling(int id);
+	//void verwijderTabel() throws SQLException ;
+	ArrayList<Bestelling> haalBestellijst();
+	
+}
