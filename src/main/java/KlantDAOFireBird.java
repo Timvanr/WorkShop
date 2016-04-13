2daaab0c2d526ac87a12c7fcab0df60818aa2c2f
Timvanr <timvanr86@gmail.com>
2016-04-13 10:08:33 +0200

    Update KlantDAOFireBird.java

--- src/main/java/KlantDAOFireBird.java
+++ src/main/java/KlantDAOFireBird.java
@@ -17,7 +17,7 @@ public class KlantDAOFireBird extends Klantbestand implements KlantDAO {
 		final String DRIVER_CLASS = "org.firebirdsql.jdbc.FBDriver";
 		final String USERNAME = "SYSDBA";
 		final String pw = "MasterKey";
-		final String URL = "jdbc:firebirdsql://localhost:3050/D:/HOST.gdb";
+		final String URL = "jdbc:firebirdsql://localhost:3050/D:/Workshop.gdb";
 		Connection connection = null;
 		
 		try{
