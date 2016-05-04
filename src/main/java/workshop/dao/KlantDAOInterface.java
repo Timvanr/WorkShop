bbe6a68bcf3d2eb98015c2b4e9cd7f4a4e024bf4
Timvanr <timvanr86@gmail.com>
2016-05-04 16:57:56 +0200

    Update KlantDAOInterface.java

--- src/main/java/workshop/dao/KlantDAOInterface.java
+++ src/main/java/workshop/dao/KlantDAOInterface.java
@@ -1,25 +1,15 @@
 package workshop.dao;
 
-import java.util.Set;
-
+import java.util.List;
 import workshop.model.Klant;
 
 public interface KlantDAOInterface {
 
-	int createKlant(Klant klant);
-	/*
-	void createKlantEnAdres(Klant klant, Adres adres);
-	void createKlantEnAdresEnBestelling(Klant klant, Adres adres, Bestelling bestelling);
-	void createKlantEnBestelling(Klant klant, Bestelling bestelling);
-	*/
-	Klant readKlantWithId(int klant_id);
-	Set<Klant> readKlantWithFirstName(String voornaam);
-	Set<Klant> readKlantByAchternaam(String achter);
-	Klant readKlantWithFirstLastName(String voornaam, String tussenvoegsel, String achternaam);
-	void UpdateKlantNaam(int klant_id, Klant klant);
-	void updateEmail(int klant_id, String email);
-	Set<Klant> readAll();
-	void deleteAllFromKlantId(int klant_id);
-	void deleteAllFromKlantNaam(String voornaam, String achternaam, String tussenvoegsel);
-	
+	long createKlant(workshop.model.Klant klant);
+	Klant readKlantWithId(long klant_id);
+	List<Klant> readAll();
+	void UpdateKlant(long klant_id, Klant klant);
+	void UpdateKlantNaam(long klant_id, Klant klant);
+	void UpdateKlantEmail(long klant_id, Klant klant);
+	void deleteAllFromKlantId(long klant_id);
 }
