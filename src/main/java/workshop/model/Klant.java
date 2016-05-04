7c7e2d00a7fd46dac0d2b6bf31b6b953cbe6545b
Timvanr <timvanr86@gmail.com>
2016-05-04 16:59:55 +0200

    Update Klant.java

--- src/main/java/workshop/model/Klant.java
+++ src/main/java/workshop/model/Klant.java
@@ -1,30 +1,67 @@
 package workshop.model;
 
-import workshop.dao.*;
-import java.sql.SQLException;
-import java.util.*;
+import java.util.Map;
+import java.util.Set;
+import javax.annotation.Resource;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.MapKeyJoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
 
-import javax.persistence.*;
 
 @Entity
-public class Klant {
-	@Id @GeneratedValue
-	@JoinTable(name="klant_has_adres", joinColumns=@JoinColumn(name="klant_id"))
-	private int klant_id;
+@Table(name= "klant")
+public class Klant implements java.io.Serializable {
+	@Id
+	@GeneratedValue (strategy=GenerationType.AUTO)
+	private long klant_id;
+	@Column
 	private String voornaam;
+	@Column
 	private String tussenvoegsel;
+	@Column
 	private String achternaam;
+	@Column
 	private String email;
-	@ManyToMany(mappedBy="bewoners")
-	private Set<Adres> adressen;		
-	
-	public Klant(){
-		this(0, null, null, null, null);
+
+
+	@Resource(name="bestellingen")
+	@OneToMany(mappedBy = "klant", targetEntity = Bestelling.class, fetch = FetchType.EAGER) 
+	@Cascade({CascadeType.SAVE_UPDATE})
+	private Set<Bestelling> bestellingen;
+	@Resource(name="Factuur")
+	@OneToMany(mappedBy = "klant", targetEntity = Factuur.class, fetch = FetchType.EAGER)
+	@Cascade({CascadeType.SAVE_UPDATE})
+	private Set<Factuur> facturen;
+	@Resource(name="accounts")
+	@OneToMany(mappedBy = "klant", targetEntity = Account.class, fetch = FetchType.EAGER)
+	@Cascade({CascadeType.SAVE_UPDATE})
+	private Set<Account> accounts;
+	@Resource(name="adressen")
+	@ManyToMany
+	@JoinTable(name="klant_has_adres", joinColumns=@JoinColumn(name="klant_id"),
+	inverseJoinColumns=@JoinColumn(name="adrestype_id"))
+	@MapKeyJoinColumn(name = "adres_id", table = "klant_has_adres")
+	private Map<Adres, AdresType> adressen;
+
+
+	public Klant() {
 	}
+
 	public Klant(String voornaam, String tussenvoegsel, String achternaam, String email){
 		this(0, voornaam, tussenvoegsel, achternaam, email);
-	}
-	
+	} 
+
 	public Klant(int id, String voornaam, String tussenvoegsel, String achternaam, String email){
 		this.klant_id = id;
 		this.voornaam = voornaam;
@@ -32,73 +69,74 @@ public class Klant {
 		this.tussenvoegsel = tussenvoegsel;
 		this.email = email;
 	}
-	
-	public static Klant get(int id){
-		KlantDAOInterface klantDAO = DAOFactory.getKlantDAO();
-		return klantDAO.readKlantWithId(id);
-	}
-	
-	public Set<Adres> getAdressen(){
-		return this.adressen;
-	}
-	
-	public void setAdressen(Set<Adres> adresSet){
-		this.adressen = adresSet;
-	}
-	
-	public String adresSetString(){
-		Set<Adres> adresSet = getAdressen();
-		String adresString = "";
-		for (Adres a: adresSet){
-			adresString += "\nAdresnummer" + a.getId() + "\n" + a.toString();
-		}
-		return adresString;
-	}
-			
-	public int getId() {
-		return this.klant_id;
-	}
 
-	public void setId(int klant_id) {
+	public long getKlant_id() {
+		return klant_id;
+	}
+	public void setKlant_id(long klant_id) {
 		this.klant_id = klant_id;
 	}
 
 	public String getVoornaam() {
-		return this.voornaam;
+		return voornaam;
 	}
-
 	public void setVoornaam(String voornaam) {
 		this.voornaam = voornaam;
 	}
-	
-	public String getTussenvoegsel() {
-		return this.tussenvoegsel;
-	}
 
+	public String getTussenvoegsel() {
+		return tussenvoegsel;
+	}
 	public void setTussenvoegsel(String tussenvoegsel) {
 		this.tussenvoegsel = tussenvoegsel;
 	}
-	
-	public String getAchternaam() {
-		return this.achternaam;
-	}
 
+	public String getAchternaam() {
+		return achternaam;
+	}
 	public void setAchternaam(String achternaam) {
 		this.achternaam = achternaam;
 	}
-	
-	public String getEmail() {
-		return this.email;
-	}
 
+	public String getEmail() {
+		return email;
+	}
 	public void setEmail(String email) {
 		this.email = email;
-	}	
-	
+	}
+
+	public Set<Bestelling> getBestellingen() {
+		return bestellingen;
+	}
+	public void setBestellingen(Set<Bestelling> bestellingen) {
+		this.bestellingen = bestellingen;
+	}
+
+	public Set<Factuur> getFacturen() {
+		return facturen;
+	}
+	public void setFacturen(Set<Factuur> facturen) {
+		this.facturen = facturen;
+	}
+
+	public Set<Account> getAccounts() {
+		return accounts;
+	}
+	public void setAccounts(Set<Account> accounts) {
+		this.accounts = accounts;
+	}
+
+	public Map<Adres, AdresType> getAdressen() {
+		return this.adressen;
+	}
+
+	public void setAdressen(Map<Adres, AdresType> adressen) {
+		this.adressen = adressen;
+	}
+
 	@Override
 	public String toString(){
-		return "Klantnummer: " + this.klant_id + "\n" + 
-				this.voornaam + " " + this.tussenvoegsel + " " + this.achternaam + 
-				adresSetString() + "\n" + this.email;
+		return this.voornaam + " " + this.tussenvoegsel + " " + this.achternaam + "\n" +  "\n" + this.email;
 	}
+
 }
