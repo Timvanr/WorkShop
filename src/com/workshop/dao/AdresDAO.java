9cb725e026f08eba336009dc4b70bf5d1110324a
Timvanr <timvanr86@gmail.com>
2016-05-24 20:21:44 +0200

    Update AdresDAO.java

--- src/com/workshop/dao/AdresDAO.java
+++ src/com/workshop/dao/AdresDAO.java
@@ -1,21 +1,10 @@
 package com.workshop.dao;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.List;
-import java.util.Scanner;
-
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 import org.springframework.stereotype.Repository;
-
 import com.workshop.model.Adres;
 
 @Repository
@@ -26,10 +15,8 @@ public class AdresDAO implements AdresDAOInterface {
 	private EntityManager em;
 	
 	@Override
-	public long createAdres(Adres adres) {
-		this.em.persist(adres);
-		return adres.getId();
-		
+	public void createAdres(Adres adres) {
+		this.em.persist(adres);		
 	}
 	
 	@Override
@@ -43,8 +30,8 @@ public class AdresDAO implements AdresDAOInterface {
 	}
 	
 	@Override
-	public void updateAdres(long id, Adres adres) {
-		Adres oud = getAdresmetId(id);
+	public void updateAdres(Adres adres){ 
+		Adres oud = getAdresmetId(adres.getAdres_id());
 		this.em.getTransaction().begin();
 		oud.setStraatnaam(adres.getStraatnaam());
 		oud.setPostcode(adres.getPostcode());
