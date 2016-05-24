491e0fe9eee2c9aafc4b3979d0949fe44e83a162
Timvanr <timvanr86@gmail.com>
2016-05-24 19:51:44 +0200

    Update AdresDAO.java

--- src/com/workshop/dao/AdresDAO.java
+++ src/com/workshop/dao/AdresDAO.java
@@ -17,91 +17,45 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.workshop.model.Adres;
+
 @Repository
+@Transactional
 public class AdresDAO implements AdresDAOInterface {
-	BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-	Scanner scInput = new Scanner(System.in);
-	protected static Logger logger = LoggerFactory.getLogger(AdresDAO.class);
 	
-	
-	private SessionFactory sessionFactory;
-	 
-	public SessionFactory getSessionFactory() {
-	return sessionFactory;
-	}
-	 
-	@Autowired
-	public void setSessionFactory(SessionFactory sessionFactory) {
-	this.sessionFactory = sessionFactory;
-	}
-	
-	public AdresDAO(){			
-	}
+	@PersistenceContext
+	private EntityManager em;
 	
 	@Override
-	public void createAdres(Adres adres){
-		Session session = sessionFactory.openSession();
-		Transaction tx = null;
-		try {
-
-			tx = session.beginTransaction();
-			session.persist(adres);
-			tx.commit();
-		}catch (HibernateException e) {
-			if (tx!=null) tx.rollback();
-			e.printStackTrace(); 
-		}finally {
-			session.close(); 
-		}
-	}
-
-	@Override
-	public List<Adres> readAll(){
-		Session session = sessionFactory.openSession();
-		List<Adres> adresList = null;
-		try{
-			adresList = (List<Adres>)session.createQuery("from Adres").list();
-		}catch(HibernateException e){
-			e.printStackTrace();
-		}finally {
-			session.close(); 
-		}
-		return adresList;
-	}
-	
-	@Override
-	public Adres readAdresmetId(long adres_id) {
-		Session session = sessionFactory.openSession();
-		Adres adres = null;
-		try {	
-			adres = (Adres) session.get(Adres.class, adres_id);
-		}catch (HibernateException e){
-			e.printStackTrace();
-		}finally {
-			session.close();
-		}
-		return adres;
-	}
-	
-	@Override
-	public void updateAdres(Adres adres){		
+	public long createAdres(Adres adres) {
+		this.em.persist(adres);
+		return adres.getId();
 		
 	}
 	
 	@Override
-	public void deleteAdres(long adres_id) {
-		Session session = sessionFactory.openSession();
-		Transaction tx = null;
-		try{ 
-			tx = session.beginTransaction();
-			Adres adres = (Adres)session.get(Adres.class, adres_id);
-			session.delete(adres);
-			tx.commit();
-		}catch (HibernateException e) {
-			if (tx!=null) tx.rollback();
-			e.printStackTrace(); 
-		}finally {
-			session.close(); 
-		}
+	public Adres getAdresmetId(long adres_id) {
+		return this.em.find(Adres.class, adres_id);
 	}
+	
+	@Override
+	public List<Adres> getAdreslijst() {
+		return this.em.createNativeQuery("SELECT * FROM ", Adres.class).getResultList();
+	}
+	
+	@Override
+	public void updateAdres(long id, Adres adres) {
+		Adres oud = getAdresmetId(id);
+		this.em.getTransaction().begin();
+		oud.setStraatnaam(adres.getStraatnaam());
+		oud.setPostcode(adres.getPostcode());
+		oud.setToevoeging(adres.getToevoeging());
+		oud.setWoonplaats(adres.getWoonplaats());
+		this.em.getTransaction().commit();
+	}
+	
+	@Override
+	public void deleteAdres(long adres_id) {
+		this.em.remove(getAdresmetId(adres_id));
+	}
+
 }
