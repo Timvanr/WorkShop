ce8081935c6bfd491c693cb8fa8972ce255e231b
Timvanr <timvanr86@gmail.com>
2016-05-24 19:41:06 +0200

    Update AccountDAO.java

--- src/com/workshop/dao/AccountDAO.java
+++ src/com/workshop/dao/AccountDAO.java
@@ -14,105 +14,40 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.workshop.model.*;
+import com.workshop.model.Account;
 
 @Repository
+@Transactional
 public class AccountDAO implements AccountDAOInterface {
-	BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-	Scanner scInput = new Scanner(System.in);
-	protected static Logger logger = LoggerFactory.getLogger(AccountDAO.class);
 	
-	private SessionFactory sessionFactory;
-	 
-	public SessionFactory getSessionFactory() {
-	return sessionFactory;
-	}
-	 
-	@Autowired
-	public void setSessionFactory(SessionFactory sessionFactory) {
-	this.sessionFactory = sessionFactory;
-	}
+	@PersistenceContext
+	private EntityManager em;
 	
-	public AccountDAO(){			
+	@Override
+	public void createAccount(Account account) {
+		this.em.persist(account);	
 	}
 	
 	@Override
-	public void createAccount(Account account){
-		Session session = sessionFactory.openSession();
-		Transaction tx = null;
-		try {
-
-			tx = session.beginTransaction();
-			session.persist(account);
-			tx.commit();
-		}catch (HibernateException e) {
-			if (tx!=null) tx.rollback();
-			e.printStackTrace(); 
-		}finally {
-			session.close(); 
-		}
+	public Account getAccountmetId(long account_id){
+		return this.em.find(Account.class, account_id);
 	}
 	
 	@Override
-	public Account readAccountmetId(long Account_id) {
-		Session session = sessionFactory.openSession();
-		Account account = null;
-		try {	
-			account = (Account) session.get(Account.class, Account_id);
-		}catch (HibernateException e){
-			e.printStackTrace();
-		}finally {
-			session.close();
-		}
-		return account;
+	public List<Account> readAll() {
+		return this.em.createNativeQuery("SELECT * FROM ", Account.class).getResultList();
 	}
 	
 	@Override
-	public List<Account> readAll(){
-		Session session = sessionFactory.openSession();
-		List<Account> accountList = null;
-		try{
-			accountList = (List<Account>)session.createQuery("from Account").list();
-		}catch(HibernateException e){
-			e.printStackTrace();
-		}finally {
-			session.close(); 
-		}
-		return accountList;
-	}
-	
-	@Override
-	public void UpdateAccount(long account_id, Account account1){
-		Session session = sessionFactory.openSession();
-		Transaction tx = null;
-		try {
-			tx = session.beginTransaction();
-			Account account = (Account)session.load(Account.class, account_id);
-			account.setNaam(account1.getNaam());
-			session.update(account); 
-			tx.commit();
-		}catch (HibernateException e) {
-			if (tx!=null) tx.rollback();
-			e.printStackTrace(); 
-		}finally {
-			session.close(); 
-		}
+	public void updateAccount(long account_id, Account account) {
+		Account oud = getAccountmetId(account_id);
+		this.em.getTransaction().begin();
+		oud.setNaam(account.getNaam());
+		this.em.getTransaction().commit();
 	}
 	
 	@Override
 	public void deleteAccount(long account_id) {
-		Session session = sessionFactory.openSession();
-		Transaction tx = null;
-		try{ 
-			tx = session.beginTransaction();
-			Account account = (Account)session.get(Account.class, account_id);
-			session.delete(account);
-			tx.commit();
-		}catch (HibernateException e) {
-			if (tx!=null) tx.rollback();
-			e.printStackTrace(); 
-		}finally {
-			session.close(); 
-		}
+		this.em.remove(getAccountmetId(account_id));
 	}
 }
