9eedea0d7b8551536ae5c735d78ad712a9be7c65
Timvanr <timvanr86@gmail.com>
2016-05-24 20:21:00 +0200

    Update AccountDAO.java

--- src/com/workshop/dao/AccountDAO.java
+++ src/com/workshop/dao/AccountDAO.java
@@ -1,19 +1,10 @@
 package com.workshop.dao;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.*;
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 import org.springframework.stereotype.Repository;
-
 import com.workshop.model.Account;
 
 @Repository
